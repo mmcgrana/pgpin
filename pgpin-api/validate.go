3413666e2189d296b9dfcc38131cbe844616862d
Mark McGranaghan <mmcgrana@gmail.com>
2014-06-02 11:40:37 -0700

    pull out validate

---
+++ pgpin-api/validate.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var emptyRegexp = regexp.MustCompile("\\A\\s*\\z")
+
+func dataValidateNonempty(f string, s string) error {
+	if emptyRegexp.MatchString(s) {
+		return &pgpinError{
+			Id:         "invalid",
+			Message:    fmt.Sprintf("field %s must be nonempty", f),
+			HttpStatus: 400,
+		}
+	}
+	return nil
+}
+
+var slugRegexp = regexp.MustCompile("\\A[a-z0-9-]+\\z")
+
+func dataValidateSlug(f string, s string) error {
+	if !slugRegexp.MatchString(s) {
+		return &pgpinError{
+			Id:         "invalid",
+			Message:    fmt.Sprintf("field %s must be of the form [a-z0-9-]+", f),
+			HttpStatus: 400,
+		}
+	}
+	return nil
+}
