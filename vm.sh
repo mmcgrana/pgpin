a70f53d08c040bab12f5c78d85810a681641a846
Mark McGranaghan <mmcgrana@gmail.com>
2014-06-03 07:01:13 -0700

    cd into app dir

--- vm.sh
+++ vm.sh
@@ -39,9 +39,16 @@ sudo -u postgres createdb pgpin-test
 cat >> /home/vagrant/.profile <<EOF
 export DEVELOPMENT_DATABASE_URL=postgres://postgres:secret@127.0.0.1:5432/pgpin-development
 export TEST_DATABASE_URL=postgres://postgres:secret@127.0.0.1:5432/pgpin-test
-export API_AUTH="client:"$(openssl rand -hex 12)
-export PGPIN_API_URL=http://\$API_AUTH@127.0.0.1:5000
 EOF
 
 # Goreman
 sudo -u vagrant -i go get github.com/mattn/goreman
+
+# Config
+cat >> /home/vagrant/.profile <<EOF
+export API_AUTH="client:"$(openssl rand -hex 12)
+export PGPIN_API_URL=http://\$API_AUTH@127.0.0.1:5000
+EOF
+
+# App dir
+cd ~/src/github.com/mmcgrana/pgpin
