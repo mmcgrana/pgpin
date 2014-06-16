94b1c4fd71c4a79a5a5000d11b17753ff384eaf0
Mark McGranaghan <mmcgrana@gmail.com>
2014-06-16 11:56:57 -0700

    Extract scheduler tests

--- pgpin-api/system_test.go
+++ pgpin-api/system_test.go
@@ -505,34 +505,3 @@ func TestWorkerPinRefreshNotReady(t *testing.T) {
 	pinOut2 := mustDataPinGet(pinIn.Id)
 	assert.Equal(t, string([]byte(pinOut1.ResultsRows)), string([]byte(pinOut2.ResultsRows)))
 }
-
-// Scheduler behavior.
-
-func TestSchedulerNoEnqueues(t *testing.T) {
-	defer clear()
-	dbIn := mustDataDbAdd("dbs-1", ConfigDatabaseUrl)
-	pinIn := mustDataPinCreate(dbIn.Id, "pins-1", "select now()")
-	mustWorkerTick()
-	pinOut1 := mustDataPinGet(pinIn.Id)
-	mustSchedulerTick()
-	mustWorkerTick()
-	pinOut2 := mustDataPinGet(pinIn.Id)
-	assert.Equal(t, pinOut1.Version, pinOut2.Version)
-}
-
-func TestSchedulerEnqueue(t *testing.T) {
-	defer clear()
-	dbIn := mustDataDbAdd("dbs-1", ConfigDatabaseUrl)
-	pinIn := mustDataPinCreate(dbIn.Id, "pins-1", "select now()")
-	mustWorkerTick()
-	pinOut1 := mustDataPinGet(pinIn.Id)
-	ConfigPinRefreshIntervalPrev := ConfigPinRefreshInterval
-	defer func() {
-		ConfigPinRefreshInterval = ConfigPinRefreshIntervalPrev
-	}()
-	ConfigPinRefreshInterval = 0
-	mustSchedulerTick()
-	mustWorkerTick()
-	pinOut2 := mustDataPinGet(pinIn.Id)
-	assert.NotEqual(t, pinOut1.Version, pinOut2.Version)
-}
